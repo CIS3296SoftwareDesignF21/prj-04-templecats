69c80f26e73d89f3ffe0ecf82bea5150ead44123
Josh Hageman <tui32922@temple.edu>
2021-11-27 20:19:07 +0000

    added connectivity to each web page. Also styled the entry form.

--- tut1.py
+++ tut1.py
@@ -17,13 +17,13 @@ mysql.init_app(app)
 def home():
     return render_template("index.html")  # some basic inline html
 
-@app.route('/community_cats')
+@app.route('/communitycats')
 def community_cats():
-    return render_template("ccats.html")
+    return render_template("communitycats.html")
 
-@app.route("/see_cats_outside")
+@app.route("/catsoutside")
 def see_cats_outside():
-    return render_template("see_cats_outside.html")
+    return render_template("catsoutside.html")
 
 @app.route("/tnr")
 def tnr():
@@ -35,7 +35,7 @@ def our_kitties():
 
 @app.route("/pet_care")
 def pet_care():
-   return render_tempate("pet_care.html")
+   return render_template("pet_care.html")
 
 @app.route("/donate_volunteer",methods=['GET','POST'])
 def donate_volunteer():
